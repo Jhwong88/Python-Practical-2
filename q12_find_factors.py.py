f4d9338d2db9962dc99fb719169c2f2f139cef48
Jhwong88 <36143501+Jhwong88@users.noreply.github.com>
2018-02-11 17:24:00 +0800

    Add files via upload

---
+++ q12_find_factors.py.py
@@ -0,0 +1,14 @@
+# Python Practical 2- to find the factors of a number
+#Done by Wong Jun Hao
+
+
+def print_factors(x):
+
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = int(input("Enter a number: "))
+
+print_factors(num)
